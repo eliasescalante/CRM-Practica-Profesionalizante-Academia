0bb3ba0208a74dc33df900d43a31a7eb42af95d6
eliasescalante <eliasw.escalante@gmail.com>
2026-08-26 21:29:26 -0300

    update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -4,16 +4,15 @@ from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 from dotenv import load_dotenv
 
-# BASE_DIR es la raíz del proyecto (donde está el .env y el index.html principal)
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 env_path = BASE_DIR / '.env'
 load_dotenv(dotenv_path=env_path)
 
-# Apuntar la carpeta de templates a la raíz del proyecto
+
 app = Flask(__name__, template_folder=str(BASE_DIR))
 CORS(app)
 
-# Configuración de base de datos
 app.config['DB_HOST'] = os.getenv('DB_HOST', 'localhost')
 app.config['DB_USER'] = os.getenv('DB_USER', 'root')
 app.config['DB_PASSWORD'] = os.getenv('DB_PASSWORD', '')
